f2f66031a48a61a65dc7973163cc44d032ef91d0
Daniel Goodrich <dannyngoodrich@gmail.com>
2020-03-15 16:57:38 -0700

    Initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+const passport = require('../config/ppConfig');
+
+router.get('/signup', function(req, res) {
+  res.render('auth/signup');
+});
+
+router.post('/signup', (req, res) => {
+  db.user.findOrCreate({
+    where: {
+      email: req.body.email
+    }, defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).then(([user, created]) => {
+    if (created) {
+      console.log('user created');
+      passport.authenticate('local', {
+        successRedirect: '/',
+        successFlash: 'Thanks for signing up!'
+      })(req, res);
+    } else {
+      console.log('email already exist');
+      req.flash('error', 'Email already exist')
+      res.redirect('/auth/signup');
+    }
+  }).catch(err => {
+    console.log('Error occured finding or creating user');
+    console.log(err)
+    req.flash('error', err.message);
+    res.redirect('/auth/signup');
+  })
+});
+
+// router.post('/signup', (req,res) => {
+//   db.user.findOrCreate({
+//     where: {
+//       email: req.body.email
+//     }, defaults: {
+//       name: req.body.name,
+//       password: req.body.password
+//     }
+//   }).then(([user, created])=> {
+//     if(created){
+//       console.log('user created')
+//       res.redirect('/');
+//     } else {
+//       console.log('email already exists')
+//       res.redirect('/auth/signup')
+//     }
+//   })
+//   .catch(err => {
+//     console.log('Error occured finding or creating user');
+//     console.log(err);
+//     res.redirect('/auth/signup');
+//   })
+// })
+
+router.get('/login', function(req, res) {
+  res.render('auth/login');
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth/login',
+  successFlash: 'Welcome',
+  failureFlash: 'invalid username or password'
+}));
+
+router.get('/logout', (req, res)=> {
+  req.logout();
+  req.flash('success', 'Smell ya later!');
+  res.redirect('/');
+});
+
+module.exports = router;
